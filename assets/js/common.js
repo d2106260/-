69738f4ba6551ee3f6ff4c2ceac190a4a169f7a3
DC <541449735@qq.com>
2020-08-05 14:57:52 +0800

    解决token失效问题

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,7 +4,24 @@ $(function() {
     var baseURL = "http://ajax.frontend.itheima.net";
     // 如下方法在实际请求发出之前调用
     $.ajaxPrefilter(function(option) {
+        // 统一处理请求头
+        // option.headers.Authorization = sessionStorage.getItem("mytoken")
+        if (option.url.indexOf("my/") !== -1) {
+            option.headers = {
+                Authorization: sessionStorage.getItem("mytoken")
+            }
+        }
         // option 表示请求配置选项, $.ajax方法的参数
         option.url = baseURL + option.url;
+
+        // 统一处理token失效的问题
+        // 该方法何时出发?请求结束后自动触发
+        option.complete = function(res) {
+            var info = res.responseJSON;
+            if (info.status === 1 && info.message === "身份认证失败！") {
+                sessionStorage.removeItem("mytoken")
+                location.href = "./login.html"
+            }
+        }
     })
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,14 +4,15 @@ $(function() {
         $.ajax({
             type: "get",
             url: "/my/userinfo",
-            headers: {
-                Authorization: sessionStorage.getItem("mytoken")
-            },
+            // 设置请求头放在common
+            // headers: {
+            //     Authorization: sessionStorage.getItem("mytoken")
+            // },
             success: function(res) {
                 if (res.status === 0) {
-                    var uname = res.data.username;
-                    var pic = res.data.user_pic;
-                    $(".username").html(uname);
+                    var uname = res.data.username
+                    var pic = res.data.user_pic
+                    $(".username").html(uname)
                     if (pic) {
                         $(".avatar").text("")
                         $(".avatar").html(`<img src="${pic}">`)
@@ -23,16 +24,18 @@ $(function() {
     }
     loadUserInfo()
 
-    // 退出事件
+    // 退出登录
     $("#quit").on("click", function() {
         layer.confirm('确定退出?', { icon: 3, title: '提示' }, function(index) {
-            layer.close(index);
-            layer.msg('您已退出');
-            sessionStorage.clear("mytoken");
+            layer.close(index)
+            layer.msg('您已退出')
+            sessionStorage.removeItem("mytoken")
             setTimeout(function() {
                 location.href = "login.html"
             }, 1000)
-        });
+        })
         $(this).parent().removeClass("layui-this")
     })
+
+
 })
